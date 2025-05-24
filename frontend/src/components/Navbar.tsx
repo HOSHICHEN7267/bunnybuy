026754e1c83f4108b0502a91ee00429be049c584
poyucheese <linoscar92@gmail.com>
2025-05-24 22:43:01 +0800

    feat: add router

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import AuthModal from "./AuthModal";
 import logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [showModal, setShowModal] = useState(false);
@@ -14,19 +15,20 @@ const Navbar = () => {
       {/* Navbar */}
       <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-4 border-b border-gray-200 text-gray-800 bg-white shadow-sm z-50">
         {/* Logo */}
-        <img
-          src={logo}
-          alt="BunnyBuy Logo"
-          className="w-28 md:w-32 cursor-pointer"
-          onClick={() => (window.location.href = "/")}
-        />
+        <Link to="/">
+          <img
+            src={logo}
+            alt="BunnyBuy Logo"
+            className="w-28 md:w-32 cursor-pointer"
+          />
+        </Link>
 
         {/* Menu */}
         <div className="hidden md:flex items-center gap-6 text-sm">
-          <a className="cursor-pointer hover:text-pink-500" onClick={() => (window.location.href = "/")}>首頁</a>
-          <a className="cursor-pointer hover:text-pink-500" onClick={() => (window.location.href = "/all-products")}>商品總覽</a>
-          <a className="cursor-pointer hover:text-pink-500" onClick={() => (window.location.href = "/about")}>關於我們</a>
-          <a className="cursor-pointer hover:text-pink-500" onClick={() => (window.location.href = "/contact")}>聯絡我們</a>
+          <Link to="/" className="hover:text-pink-500">首頁</Link>
+          <Link to="/all-products" className="hover:text-pink-500">商品總覽</Link>
+          <Link to="/about" className="hover:text-pink-500">關於我們</Link>
+          <Link to="/contact" className="hover:text-pink-500">聯絡我們</Link>
         </div>
 
         {/* 登入按鈕 */}
