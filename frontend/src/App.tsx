55ee8b294322303a53eeec87d4d939491997abd5
harrykao0803 <kao10813@gmail.com>
2025-05-26 16:52:34 +0800

    fix PR

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
 // import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import HeaderSlider from './components/HeaderSlider'
 
 import HomePage from './pages/HomePage';
 import OrderPoolPage from './pages/OrderPoolPage';
