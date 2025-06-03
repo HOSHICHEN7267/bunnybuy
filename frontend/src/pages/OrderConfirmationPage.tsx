b7efc0953a8f406654cebb7b43bcdf0e60f6179b
chu-ching-liang <r13922193@ntu.edu.tw>
2025-06-03 18:25:13 +0800

    feat:商品狀態更新

--- frontend/src/pages/OrderConfirmationPage.tsx
+++ frontend/src/pages/OrderConfirmationPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useNavigate, Link } from "react-router-dom";
+import { useLocation, useNavigate} from "react-router-dom";
 import axios from "axios";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
