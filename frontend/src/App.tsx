215d4958059306dccaabdbd4023d7fd33ba0cbac
chu-ching-liang <r13922193@ntu.edu.tw>
2025-06-03 16:38:58 +0800

    feat:可以選擇要接的商品了

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,6 +27,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/orderconfirm" element={<OrderConfirmationPage />} />
 
         {/* PrivateRoutes */}
         <Route
